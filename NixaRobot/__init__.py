ca1dc63936e4818d0f1ff9e70eb598ee989fac0b
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-06-08 23:47:11 +0530

    Update __init__.py

--- NixaRobot/__init__.py
+++ NixaRobot/__init__.py
@@ -189,7 +189,7 @@ else:
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("nixa", API_ID, API_HASH)
-pbot = Client("ameliapbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot = Client("nixapbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
