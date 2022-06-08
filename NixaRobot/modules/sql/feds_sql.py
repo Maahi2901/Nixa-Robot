9f6b657dcf248bf6d6d531aeea46cf1035bf13bc
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-06-08 11:28:55 +0530

    Update feds_sql.py

--- NixaRobot/modules/sql/feds_sql.py
+++ NixaRobot/modules/sql/feds_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from AmeliaRobot import dispatcher
-from AmeliaRobot.modules.sql import BASE, SESSION
+from NixaRobot import dispatcher
+from NixaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
