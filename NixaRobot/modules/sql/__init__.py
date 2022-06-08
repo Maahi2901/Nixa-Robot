7d3e348e7985d3ab2531ab28a57a3760a9600181
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-06-08 11:29:30 +0530

    Update __init__.py

--- NixaRobot/modules/sql/__init__.py
+++ NixaRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from AmeliaRobot import DB_URI
+from NixaRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
