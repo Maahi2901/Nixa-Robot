e2a190b1d16ae56f405bf2f3f91d5685fe4611c8
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-06-09 08:10:45 +0530

    Update logger.py

--- NixaRobot/utils/logger.py
+++ NixaRobot/utils/logger.py
@@ -1,18 +1,3 @@
-# This file is part of Daisy (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import logging
 
 from loguru import logger
@@ -40,11 +25,11 @@ class InterceptHandler(logging.Handler):
 logging.basicConfig(handlers=[InterceptHandler()], level=logging.INFO)
 log = logging.getLogger(__name__)
 logger.add(
-    "logs/asuna.log",
+    "logs/nixa.log",
     rotation="1 d",
     compression="tar.xz",
     backtrace=True,
     diagnose=True,
     level="INFO",
 )
-log.info("Enabled logging intro amelia.log file.")
+log.info("» ᴇɴᴀʙʟᴇᴅ ʟᴏɢɢɪɴɢ ɪɴᴛʀᴏ ɴɪxᴀ.ʟᴏɢ ғɪʟᴇ.")
