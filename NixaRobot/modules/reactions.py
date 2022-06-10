4a3f788a8531a65b33af17fb4bb31335d5fb7d74
Maahi2901 <107261681+Maahi2901@users.noreply.github.com>
2022-06-10 22:04:55 +0530

    Update reactions.py

--- NixaRobot/modules/reactions.py
+++ NixaRobot/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from AmeliaRobot import dispatcher
-from AmeliaRobot.modules.disable import DisableAbleCommandHandler
+from NixaRobot import dispatcher
+from NixaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
