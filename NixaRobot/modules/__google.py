20a4eb3f8158210f60e9ad941daf05057f2c5fd1
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-06-10 19:56:13 +0530

    Update __google.py

--- NixaRobot/modules/__google.py
+++ NixaRobot/modules/__google.py
@@ -267,7 +267,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Yone <==="
+        app_details += "\n\n===> ɴɪxᴀ <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "◎Search"
+__mod_name__ = "sᴇᴀʀᴄʜ"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
@@ -286,6 +286,6 @@ __help__ = """
  ❍ /github <username>*:* Get information about a GitHub user.
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
- ❍ Amelia <query>*:* Amelia answers the query
-  💡Ex: `Amelia where is India?`
+ ❍ Nixa <query>*:* Nixa answers the query
+  💡Ex: `Nixa where is India?`
 """
