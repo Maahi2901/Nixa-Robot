7b40d9c6253b786a915c25edbde330c7f0aff9a4
Maahi2901 <107261681+Maahi2901@users.noreply.github.com>
2022-06-10 22:05:43 +0530

    Update remote_cmds.py

--- NixaRobot/modules/remote_cmds.py
+++ NixaRobot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from AmeliaRobot import dispatcher
-from AmeliaRobot.modules.helper_funcs.chat_status import (
+from NixaRobot import dispatcher
+from NixaRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from AmeliaRobot.modules.helper_funcs.extraction import extract_user_and_text
-from AmeliaRobot.modules.helper_funcs.filters import CustomFilters
+from NixaRobot.modules.helper_funcs.extraction import extract_user_and_text
+from NixaRobot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
