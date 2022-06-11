0e996d2c1cca2eaf9a1e75a929f1b63cca539298
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-06-11 09:48:06 +0530

    Update voicechatbot.py

--- NixaRobot/modules/voicechatbot.py
+++ NixaRobot/modules/voicechatbot.py
@@ -3,7 +3,7 @@ import aiofiles
 import aiohttp
 from random import randint
 from pyrogram import filters
-from NixaRobot import pbot as LYCIA
+from NixaRobot import pbot as Nixa
 
 async def fetch(url):
     async with aiohttp.ClientSession() as session:
@@ -14,8 +14,8 @@ async def fetch(url):
                 data = await resp.text()
     return data
 
-async def ai_amelia(url):
-    ai_name = "Amelia.mp3"
+async def ai_nixa(url):
+    ai_name = "nixa.mp3"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             if resp.status == 200:
@@ -25,25 +25,25 @@ async def ai_amelia(url):
     return ai_name
 
 
-@LYCIA.on_message(filters.command("Amelia"))
+@Nixa.on_message(filters.command("Nixa"))
 async def Lycia(_, message):
     if len(message.command) < 2:
-        await message.reply_text("Lycia AI Voice Chatbot")
+        await message.reply_text("Nixa AI Voice Chatbot")
         return
     text = message.text.split(None, 1)[1]
-    amelia = text.replace(" ", "%20")
-    m = await message.reply_text("Amelia Is Best...")
+    Nixa = text.replace(" ", "%20")
+    m = await message.reply_text("Nixa Is Best...")
     try:
         L = await fetch(f"https://api.affiliateplus.xyz/api/chatbot?message={lycia}&botname=amelia&ownername=Abhishek&user=1")
         chatbot = L["message"]
         VoiceAi = f"https://lyciavoice.herokuapp.com/lycia?text={chatbot}&lang=hi"
-        name = "amelia"
+        name = "nixa"
     except Exception as e:
         await m.edit(str(e))
         return
-    await m.edit("Made By @itsmelegend...")
-    LyciaVoice = await ai_lycia(VoiceAi)
+    await m.edit("Made By @Simple_Mundaa...")
+    NixaVoice = await ai_Nixa(VoiceAi)
     await m.edit("Repyping...")
-    await message.reply_audio(audio=LyciaVoice, title=chatbot, performer=name)
-    os.remove(LyciaVoice)
+    await message.reply_audio(audio=NixaVoice, title=chatbot, performer=name)
+    os.remove(NixaVoice)
     await m.delete()
