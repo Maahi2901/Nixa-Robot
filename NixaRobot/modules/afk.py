686329df906f59da5c9913a755b972a406a2912b
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-06-11 09:35:56 +0530

    Update afk.py

--- NixaRobot/modules/afk.py
+++ NixaRobot/modules/afk.py
@@ -23,7 +23,7 @@ def afk(update: Update, context: CallbackContext):
     if not user:  # ignore channels
         return
 
-    if user.id in [777000, 1087968824]:
+    if user.id in [5576613955, 5331796378]:
         return
 
     notice = ""
@@ -151,7 +151,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "AFK"
+__mod_name__ = "ᴀғᴋ"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
